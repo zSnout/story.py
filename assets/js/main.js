a2a816949f2b149dd14abd8a124c422d23c621e4
zsnout-zs <65452710+zsnout-zs@users.noreply.github.com>
2020-07-19 10:43:05 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,103 @@
 class Page {
   static get pages() {
+    var pages = [];
+    
     for (var member in window) {
       if (window[member] instanceof Page) {
-        
+        pages.push(window[member]);
       }
     }
+    
+    return pages;
   }
   
   constructor(url,title,group,keyword) {
     this.url = url;
     this.title = title;
     this._groups = group;
-    this._keyword = keyword;
-    
-    
+    this._keywords = keyword;
   }
   
   get groups() {
     var group = this._groups.split(/ *, */g);
     
     for (var j = 0;j < group.length;j++) {
-      group[j] = group[j].replace(/-/g," ");
-      
-      var match = group[j].match(/(.+) *\/ *(.+) *\/ *(.+)/);
-      if (match) {
-        group[j] = {
-          type: "subsub",
-          root: match[1],
-          sub: match[2],
-          subsub: match[3]
-        };
-      } else {
-        var match = group[j].match(/(.+) *\/ *(.+)/);
-        if (match) {
-          group[j] = {
-            type: "sub",
-            root: match[1],
-            sub: match[2]
-          };
-        } else {
-          group[j] = {
-            type: "root",
-            root: group[j]
-          }
-        }
-      }
+      group[j] = new Group(group[j]);
     }
     
     return group;
+  }
+  
+  get keywords() {
+    var keyword = this._keywords.split(/ +/g);
+    
+    for (var j = 0;j < keyword.length;j++) {
+      keyword[j] = new Keyword(keyword[j]);
+    }
+    
+    return keyword;
+  }
+}
+
+class Group {
+  constructor(name) {
+    this._name = name.replace(/-/g," ");
+  }
+  
+  get type() {
+    if (this.name.match(/.+\/.+\/.+/)) {
+      return "subsub";
+    } else if (this.name.match(/.+\/.+/)) {
+      return "sub";
+    } else {
+      return "root";
+    }
+  }
+  
+  get name() {
+    var match = this._name.match(/^(.+?)(?: *\/ *(.+?)(?: *\/ *(.+?))?)?$/);
+    
+    return {
+      orig: match[0],
+      root: match[1],
+      sub: match[2] || null,
+      subsub: match[3] || null
+    };
+  }
+  
+  get pages() {
+    var pages = Page.pages;
+    
+    var myPages = [];
+    for (var i = 0;i < pages.length;i++) {
+      if (pages[i].groups.map(elem => elem.name.orig).indexOf(this._name) != -1) {
+        myPages.push(pages[i]);
+      }
+    }
+    
+    return pages;
+  }
+}
+
+class Keyword {
+  constructor(keyword) {
+    this._keyword = keyword;
+  }
+  
+  get keyword() {
+    return this._keyword;
+  }
+  
+  get pages() {
+    var pages = Page.pages;
+    
+    var myPages = [];
+    for (var i = 0;i < pages.length;i++) {
+      if (pages[i].keywords.map(elem => elem.keyword).indexOf(this._keyword) != -1) {
+        myPages.push(pages[i]);
+      }
+    }
+    
+    return pages;
   }
 }
